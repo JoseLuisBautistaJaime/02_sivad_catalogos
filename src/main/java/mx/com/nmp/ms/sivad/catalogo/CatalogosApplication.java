d17ade8e2e9d0137c186e4d80959432c749c9815
osanchez <osanchez@d0c2015e-0c26-44a3-b760-eac3cb1bf761>
2016-11-07 05:50:00 +0000

    [sivad cat] * Se muestra información de la aplicación al iniciar

--- src/main/java/mx/com/nmp/ms/sivad/catalogo/CatalogosApplication.java
+++ src/main/java/mx/com/nmp/ms/sivad/catalogo/CatalogosApplication.java
@@ -4,6 +4,8 @@
  */
 package mx.com.nmp.ms.sivad.catalogo;
 
+import mx.com.nmp.ms.arquetipo.profile.DefaultProfileUtil;
+import mx.com.nmp.ms.arquetipo.profile.NmpProfile;
 import mx.com.nmp.ms.sivad.catalogo.config.AppProperties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,7 +16,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Collection;
 
 /**
  * Aplicación SpringBoot - Microservicio de Catálogos.
@@ -30,13 +40,44 @@ public class CatalogosApplication {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(CatalogosApplication.class);
 
+    @Inject
+    private Environment env;
+
+    /**
+     * Despues de inicializacion.
+     * <p>
+     * Se pueden especificar perfiles como argumentos de línea de comandos: --spring.profiles.active=el-perfil-activo
+     */
+    @PostConstruct
+    public void initApplication() {
+        LOGGER.info("Ejecutando con perfil(es) de Spring : {}", Arrays.toString(env.getActiveProfiles()));
+        Collection<String> activeProfiles = Arrays.asList(env.getActiveProfiles());
+        if (activeProfiles.contains(NmpProfile.DEV) && activeProfiles.contains(NmpProfile.PROD)) {
+            LOGGER.error("Hay un problema de configuración. No deberían estar presentes los perfiles Dev y Prod a la vez.");
+        }
+    }
+
     /**
      * Metodo principal de la aplicacion.
      * @param args Lista de parametros.
      */
-    public static void main(String[] args) {
-        LOGGER.info(">> main");
-        SpringApplication.run(CatalogosApplication.class, args);
-        LOGGER.info("<< main");
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(CatalogosApplication.class);
+        DefaultProfileUtil.addDefaultProfile(app);
+
+        Environment env = app.run(args).getEnvironment();
+        LOGGER.info("\n----------------------------------------------------------\n\t" +
+                "Aplicacion '{}' en ejecucion. URLs:\n\t" +
+                "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                "Externa: \thttp://{}:{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            env.getProperty("server.port"),
+            InetAddress.getLocalHost().getHostAddress(),
+            env.getProperty("server.port"));
+
+        String configServerStatus = env.getProperty("configserver.status");
+        LOGGER.info("\n----------------------------------------------------------\n\t" +
+                "Servidor de Configuracion: \t{}\n----------------------------------------------------------",
+            configServerStatus == null ? "No encontrado o no establecido." : configServerStatus);
     }
 }
