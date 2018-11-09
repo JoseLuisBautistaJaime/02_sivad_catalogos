17557a361039d4478eb435a51e7d23a2e3f5b2b3
Jorge Galvez <jgalvez@quarksoft.net>
2018-11-08 21:35:20 -0600

    * * Nombre Proyecto: Siva Diamantes F2 Nombre Requerimiento: Se agrega configuracion de max numero de conexiones.

--- src/main/java/mx/com/nmp/ms/sivad/catalogo/config/WebConfigurer.java
+++ src/main/java/mx/com/nmp/ms/sivad/catalogo/config/WebConfigurer.java
@@ -14,8 +14,15 @@ import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletCont
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.embedded.MimeMappings;
 import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
 import javax.inject.Inject;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -27,7 +34,7 @@ import javax.servlet.ServletRegistration;
  * @author osanchez
  */
 @Configuration
-public class WebConfigurer implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
+public class WebConfigurer extends WebMvcConfigurerAdapter implements ServletContextInitializer, EmbeddedServletContainerCustomizer {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(WebConfigurer.class);
 
@@ -36,6 +43,9 @@ public class WebConfigurer implements ServletContextInitializer, EmbeddedServlet
 
     @Inject
     private AppProperties appProperties;
+    
+    @Inject
+    private ApplicationProperties applicationProperties;
 
     @Autowired(required = false)
     private MetricRegistry metricRegistry;
@@ -70,6 +80,30 @@ public class WebConfigurer implements ServletContextInitializer, EmbeddedServlet
         h2ConsoleServlet.addMapping("/h2-console/*");
         h2ConsoleServlet.setInitParameter("-properties", "src/main/resources/");
         h2ConsoleServlet.setLoadOnStartup(1);
+    }
+
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = applicationProperties.getCors();
+        if (config.getAllowedOrigins() != null && !config.getAllowedOrigins().isEmpty()) {
+        	LOGGER.info("Registrando filtro CORS..." + config.getAllowedOrigins());
+            source.registerCorsConfiguration("/**", config);
+        }
+        CorsFilter corsFilter = new CorsFilter(source);
+        return corsFilter;
+    }
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+    	LOGGER.info("Realizando el registro de filtros CORS desde Adapter...");
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("PUT", "DELETE", "POST", "GET")
+                .allowedHeaders("*")
+                .allowCredentials(false).maxAge(3600);
     }
 
 }
