2fa0f0383c788f605f7ddfecfa43214a9cdf9eea
Marco Delgadillo Escobar <madelgadillo@quarksoft.net>
2019-04-04 18:18:40 -0600

    Nombre de Proyecto: Sistema de Operación Prendaria Emergente - Catalogos Observaciones: Se agrego el catálogo de reportes

---
+++ src/main/java/mx/com/nmp/ms/sivad/catalogo/repository/ReportesRepository.java
@@ -0,0 +1,14 @@
+
+package mx.com.nmp.ms.sivad.catalogo.repository;
+
+import mx.com.nmp.ms.sivad.catalogo.domain.Reportes;
+import org.springframework.stereotype.Repository;
+
+/**
+ * Interfaz que expone métodos para consultar el catálogo de reportes
+ * @author madelgadillo
+ */
+@Repository
+public interface ReportesRepository extends CatalogoRepository<Reportes>{
+    
+}
